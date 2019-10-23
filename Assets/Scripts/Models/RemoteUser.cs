6aedf8c09a4f121af45280425f3184db099b5ced
shuttler67 <shuttler67@gmail.com>
2019-10-23 14:03:07 +0200

    Add lerping of the remote user positions

--- Assets/Scripts/Models/RemoteUser.cs
+++ Assets/Scripts/Models/RemoteUser.cs
@@ -6,9 +6,24 @@ using UnityEngine;
 namespace Assets.Scripts.Models {
     public class RemoteUser : MonoBehaviour
     {
+
+
+        private IEnumerator LerpToPosition(Vector3 position, float time)
+        {
+            float elapsedTime = 0;
+            Vector3 previousPosition = transform.localPosition;
+
+            while (elapsedTime < time)
+            {
+                transform.localPosition = Vector3.Lerp(previousPosition, position, elapsedTime/time);
+                elapsedTime += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
         public void UpdatePosition(Position newPos)
         {
-            transform.localPosition = newPos.Coordinates.convertCoordinateToVector(transform.position.y);
+            StartCoroutine(LerpToPosition( newPos.Coordinates.convertCoordinateToVector(transform.position.y), 0.5f));
         }
     }
 }
