0ee6fa7cf43af4949f9b192e7d829ac080b8be2e
gsorensen <gsorensen1201@gmail.com>
2020-01-21 09:52:51 +0100

    Added a first draft of a dropdown menu where we can go back to the main menu

---
+++ Assets/Scripts/DropDownMenuUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DropDownMenuUtility : MonoBehaviour
+{
+    public Dropdown dropdownMenuButton;
+    public int menuIndex;
+    void Start() 
+    {
+        dropdownMenuButton.onValueChanged.AddListener(delegate {
+            DropdownMenuButtonValueChangedHandler(dropdownMenuButton);
+        });
+    }
+    void Destroy() 
+    {
+        dropdownMenuButton.onValueChanged.RemoveAllListeners();
+    }
+    
+    private void DropdownMenuButtonValueChangedHandler(Dropdown target) 
+    {
+        Debug.Log("selected: "+target.value);
+
+        // TODO Make this a switch case if we are supposed to load a scene on any choice
+        if (target.value == menuIndex)
+        {
+            SceneManager.LoadScene(sceneBuildIndex: 0);
+        }
+    }
+    
+    public void SetDropdownIndex(int index) 
+    {
+        dropdownMenuButton.value = index;
+    }
+}
+
