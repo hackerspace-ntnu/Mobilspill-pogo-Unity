4179e54f71d7b4196718119f387cdffc58bbe4d5
shuttler67 <shuttler67@gmail.com>
2020-01-21 20:51:17 +0100

    Add Hackpoint model and spawning of hackpoint.

--- Assets/Scripts/Firebase/HackpointManager.cs
+++ Assets/Scripts/Firebase/HackpointManager.cs
@@ -14,39 +14,48 @@ namespace Assets.Scripts.Firebase
 {
     public class HackpointManager : MonoBehaviour
     {
+        public GameObject hackpointPrefab;
         private static DatabaseReference database = RealtimeDatabaseManager.Instance.DBReference;
         private static DatabaseReference hackpoints = database.Child("hackpoints");
 
 
         private static Task<HackpointData> RetrieveHackpointData(string ID)
         {
-            return hackpoints.Child(ID).GetValueAsync().ContinueWith(
+            return hackpoints.Child(ID).GetValueAsync().LogErrorOrContinueWith(
                     t => {
-                        if (t.IsFaulted)
-                        {
-                            Debug.LogFormat("[HackpointDatabase] Failed retrieving hackpointdata for hackpoint {0}. {1}", ID, t.Exception);
-                        }
-                        else if (t.IsCompleted)
-                        {
-                            string json = t.Result.GetRawJsonValue();
-                            //Debug.Log("Retrieved: "+json);
-                            var data = JsonConvert.DeserializeObject<HackpointData>(json);
-                            data.ID = ID;
-                            return data;
-                        }
-                        return new HackpointData() {ID = ID};
+                        string json = t.Result.GetRawJsonValue();
+                        //Debug.Log("Retrieved: "+json);
+                        var data = JsonConvert.DeserializeObject<HackpointData>(json);
+                        data.ID = ID;
+                        return data;
+                    }
+                );
+        }
+
+        private static Task<Dictionary<string, HackpointData>> RetrieveAllHackpoints()
+        {
+            return hackpoints.GetValueAsync().LogErrorOrContinueWith(
+                    t => {
+                        string json = t.Result.GetRawJsonValue();
+                        //Debug.Log("Retrieved: "+json);
+                        return JsonConvert.DeserializeObject<Dictionary<string, HackpointData>>(json);
                     }
                 );
         }
 
         IEnumerator Start()
         {
-            var task = RetrieveHackpointData("h1");
+            var task = RetrieveAllHackpoints();
             yield return UtilityFunctions.RunTaskAsCoroutine(task);
 
-            Debug.Log(task.Result.HighscoringTeam);
-            Debug.Log(task.Result.Highscore);
-            Debug.Log(task.Result.Position);
+            var hackpoints = task.Result;
+
+            foreach(var hackpoint in hackpoints)
+            {
+                Debug.Log(hackpoint.Key);
+                var pos = hackpoint.Value.Position.Coordinates.convertCoordinateToVector(10);
+                Instantiate(hackpointPrefab, pos, Quaternion.Euler(-90,0,0));
+            }
         }
     }
 }
--- Assets/Scripts/UtilityFunctions.cs
+++ Assets/Scripts/UtilityFunctions.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using UnityEngine;
 using System.Threading.Tasks;
+using System;
 
-public class UtilityFunctions
+public static class UtilityFunctions
 {
 
     public static IEnumerator RunTaskAsCoroutine(Task task)
@@ -20,5 +21,54 @@ public class UtilityFunctions
         }
 
         yield return task;
+    }
+
+    public static Task LogErrorOrContinueWith(this Task task, Action<Task> continuationFunction)
+    {
+        return task.ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                Debug.LogWarningFormat("Task error: {0}", t.Exception);
+            }
+            else if (t.IsCanceled)
+            {
+                Debug.LogWarningFormat("Task cancelled: {0}", t.Exception);
+            }
+            else {
+                continuationFunction.Invoke(t);
+            }
+        });
+    }
+    public static Task<TResult> LogErrorOrContinueWith<TResult>(this Task task, Func<Task, TResult> continuationFunction)
+    {
+        return task.ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                Debug.LogWarningFormat("Task error: {0}", t.Exception);
+            }
+            else if (t.IsCanceled)
+            {
+                Debug.LogWarningFormat("Task cancelled: {0}", t.Exception);
+            }
+            return continuationFunction.Invoke(t);
+        });
+    }
+
+    public static Task<TNewResult> LogErrorOrContinueWith<TResult,TNewResult>(this Task<TResult> task,Func<Task<TResult>, TNewResult> continuationFunction)
+    {
+        return task.ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                Debug.LogWarningFormat("Task error: {0}", t.Exception);
+            }
+            else if (t.IsCanceled)
+            {
+                Debug.LogWarningFormat("Task cancelled: {0}", t.Exception);
+            }
+            return continuationFunction.Invoke(t);
+        });
     }
 }
