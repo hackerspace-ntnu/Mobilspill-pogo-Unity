c989610e8b9a563fea5d617bc9f8f6fc4ad2e556
shuttler67 <shuttler67@gmail.com>
2020-02-18 08:47:33 +0100

    simple changes

--- Assets/Scripts/MinigameScene.cs
+++ Assets/Scripts/MinigameScene.cs
@@ -13,20 +13,29 @@ namespace Assets.Scripts
     
         public Params sceneParams;
 
-        public static void LoadMinigameScene(Params sceneParams, System.Action<Outcome> callback) {
+        public static void LoadMinigameScene(Params sceneParams, System.Action<Outcome> callback) 
+        {
             MinigameScene.loadSceneRegister = sceneParams;
             sceneParams.callback = callback;
             SceneManager.LoadScene(sceneParams.sceneName, LoadSceneMode.Additive);
         }
 
-        public void Awake() {
-            if (loadSceneRegister != null) sceneParams = loadSceneRegister;
+        public void Awake() 
+        {
+            if (loadSceneRegister != null)
+            {
+                sceneParams = loadSceneRegister;
+            }
             loadSceneRegister = null; // the register has served its purpose, clear the state
         }
 
-        public void EndScene (Outcome outcome) {
+        public void EndScene (Outcome outcome) 
+        {
             SceneManager.UnloadSceneAsync(sceneParams.sceneName);
-            if (sceneParams.callback != null) sceneParams.callback(outcome);
+            if (sceneParams.callback != null) 
+            {
+                sceneParams.callback(outcome);
+            }
             sceneParams.callback = null; // Protect against double calling;
         }
 
