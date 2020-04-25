05865c116b0e3e058e88a7a366dc2ab9e33a0f67
Havard-SL <55786766+Havard-SL@users.noreply.github.com>
2020-04-25 14:29:40 +0200

    Added the database cleanut https request to the game (#4)
    Good

---
+++ Assets/Scripts/DatabaseCleanup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DatabaseCleanup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SendIt());
+    }
+    IEnumerator SendIt() {
+        UnityWebRequest www = UnityWebRequest.Get("https://us-central1-pogo-65145.cloudfunctions.net/updateDatabase");
+	yield return www.SendWebRequest();
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+}
